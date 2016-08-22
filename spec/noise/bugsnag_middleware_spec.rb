0748ee27cda719e7bc176fd347db50bfa7e68d86
Artem Bolshakov <abolshakov@spbtv.com>
2016-08-22 16:32:40 +0300

    ensure user name is taken from request id

--- spec/noise/bugsnag_middleware_spec.rb
+++ spec/noise/bugsnag_middleware_spec.rb
@@ -7,12 +7,14 @@ require 'support/sleanup_notification'
 RSpec.describe Noise::BugsnagMiddleware do
   let(:bugsnag) { double('bugsnag') }
   let(:ip_address) { '66.66.66.66' }
+  let(:request_id) { SecureRandom.uuid }
   let(:env) do
     {
       'HTTP_X_FORWARDED_FOR' => ip_address,
       'client_id' => 'android',
       'client_version' => '1.0.0',
       'user_email' => 'papadopoulos@example.com',
+      'action_dispatch.request_id' => request_id,
     }
   end
   let(:error) { RuntimeError.new('oops') }
@@ -28,7 +30,11 @@ RSpec.describe Noise::BugsnagMiddleware do
 
   it 'adds information to bugsnag notification' do
     expect(bugsnag).to receive(:call) do |notification|
-      expect(notification.user).to include('id' => '66.66.66.66', 'email' => 'papadopoulos@example.com')
+      expect(notification.user).to eq(
+        'id' => '66.66.66.66',
+        'email' => 'papadopoulos@example.com',
+        'name' => request_id,
+      )
       expect(notification.meta_data[:api_client]).to eq('client_id' => 'android', 'client_version' => '1.0.0')
       expect(notification.severity).to eq('error')
     end
